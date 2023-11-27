c294c98f0e8add13eddd7a1eadb5b3e9c3c19b9d
Cryptology3301 <152042681+iRenan2K@users.noreply.github.com>
2023-11-27 04:35:58 -0300

    Upgrading Simple Calculators folder

---
+++ C/Math/Simple Calculators/Simple Calculator (alpha)/calculatorA.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(){
+
+    char operator;
+    double n1, n2, result;
+
+    printf("\nHello! Welcome to the simple calculator program!\n");
+
+    printf("\nEnter what operator will be used (+) (-) (*) (/): ");
+    scanf(" %c", &operator);        // Added space before %c to consume any blank characters
+    printf("\nEnter number 1: ");
+    scanf("%lf", &n1);
+
+    printf("Enter number 2: ");
+    scanf("%lf", &n2);
+
+    switch(operator){
+        case '+':
+            result = n1 + n2;
+            printf("\nResult: %lf\n", result);
+            break;
+        case '-':
+            result = n1 - n2;
+            printf("\nResult: %lf\n", result);
+            break;
+        case '*':
+            result = n1 * n2;
+            printf("\nResult: %lf\n", result);
+            break;
+        case '/':
+            if (n2 == 0){
+                printf("\nInvalid Division! Cannot divide by zero\n");
+            } else {
+                result = n1 / n2;
+                printf("\nResult: %lf\n", result);
+            }
+            break;
+        default:
+            printf("\nEnter a valid operator!\n");
+    }
+
+    return 0;
+}
