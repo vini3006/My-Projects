379ab9b7dc62b8ac957ce6de2738cb80e7fd6bee
Cryptology3301 <152042681+iRenan2K@users.noreply.github.com>
2023-12-07 03:58:27 -0300

    Including functions and arguments folders

---
+++ C/Tutorial/Arguments/arguments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void birthday(char name[], int age){
+
+    printf("\nHappy birthday, dear %s!", name);
+    printf("\nYou are %d years old!\n", age);
+}
+
+int main(){
+    char name[] = "Bro";
+    int age = 21;
+
+    birthday(name, age);
+
+
+    return 0;
+
+}
