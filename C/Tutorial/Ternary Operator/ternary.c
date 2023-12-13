c08f95a5e7c1afcc5313ee8024f3b1f96ca94bde
Cryptology3301 <152042681+iRenan2K@users.noreply.github.com>
2023-12-12 22:26:11 -0300

    Including Return Statement, Ternary Operator folders

---
+++ C/Tutorial/Ternary Operator/ternary.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int findMax(int x, int y){
+
+    /* instead of:
+    
+        if(x > y){
+            return x;
+        }
+        else{
+            return y;
+        }
+
+        use:
+    */
+
+   return (x > y) ? x : y;
+}
+
+int main(){
+
+    // ternary operator = shortcut to if/else when assigning/returning a value
+    // (condition) ? [value if true] : [value if false]
+
+    int max = findMax(25, 36);
+    printf("%d", max);
+
+    
+    return 0;
+}
